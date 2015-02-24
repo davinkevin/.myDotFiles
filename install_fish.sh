bc374d64af2edd4fc6b6da66ea8eeade396fabbe
Davin Kevin <davin.kevin@gmail.com>
2015-02-24 07:47:41 +0100

    feat(fish): add installer to create specific symlink. Linked to #1 just for fish

---
+++ install_fish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from fish folder and the git fish folder
+############################
+
+########## Variables
+
+dir=${HOME}/.myDotFiles                    # dotfiles directory
+olddir=${HOME}/.myDotFiles/.backup             # old dotfiles backup directory
+git_fish_config=$dir/.config/fish
+fish_installation=${HOME}/.config/fish
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing fish configuration"
+mkdir -p $olddir
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Backuping old fish config to $olddir"
+rsync -avq --remove-source-files --force ${HOME}/.config/fish $olddir
+echo "Creating symlink to $git_fish_config in home directory."
+ln -s git_fish_config fish_installation
