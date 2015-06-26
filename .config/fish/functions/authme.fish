b1c0fc38d0c0420033c8d4fe9c2635edf24d6906
Davin Kevin <davinkevin@users.noreply.github.com>
2015-06-26 15:37:49 +0200

    fix(fish): correction of interpretation error in fish

--- .config/fish/functions/authme.fish
+++ .config/fish/functions/authme.fish
@@ -1,4 +1,4 @@
 function authme
-        ssh $argv 'mkdir -p ~/.ssh and cat >> ~/.ssh/authorized_keys'< ~/.ssh/id_rsa.pub
+        ssh $argv 'mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys'< ~/.ssh/id_rsa.pub;
         ssh $argv
 end
