d2d6e3e88143b573a498e353ef400bd1e49d50bc
Davin Kevin <davin.kevin@gmail.com>
2016-10-20 04:58:42 +0200

    feat(node): add node_modules from current directory in path to avoid global install

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,7 @@
 set fish_greeting ""
+
+# Set PATH : 
+set -gx PATH $PATH ./node_modules/.bin/
+
+# Autojump
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
