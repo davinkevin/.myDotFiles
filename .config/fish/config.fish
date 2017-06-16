e8e4c879c6561fb2728e17c8dfa7a95dc036b68a
Kevin Davin <kevin@iMac-de-Kevin.local>
2017-06-16 18:35:14 +0200

    feat(java): add script to install java

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ set -gx PATH $PATH ./node_modules/.bin/
 
 # Autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
---
+++ install/java.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+echo "Install last version of Java"
+brew update
+brew cask install java
