0cc714b5ba138efb288d3b5d255f79e84d56db4c
davinkevin <davin.kevin@gmail.com>
2017-06-18 08:43:44 +0200

    feat(git): add git-so-fancy to git configuration

--- install/git.fish
+++ install/git.fish
@@ -1,9 +1,26 @@
 #!/usr/bin/env fish
 
+#######
+# Git #
+#######
+
 echo "Install/Upgrade of git"
 brew update
 brew install git
 
+
+#################
+# Git Diff tool #
+#################
+
+echo "Install diff-so-fancy"
+brew install diff-so-fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+
+#####################
+# Git GPG Signature #
+#####################
 
 echo "Configuration of signed commit with GIT"
 
@@ -14,3 +31,6 @@ echo "Documentation about the whole process available here : https://gist.github
 git config --global gpg.program /usr/local/MacGPG2/bin/gpg2
 git config --global user.signingkey (gpg2 --list-secret-keys | grep sec | grep expire | head -n 1 | sed -e 's@.*R/\([^ ]*\) .*@\1@g')
 git config --global commit.gpgsign true
+
+echo "Allow to be used without GUI"
+echo "no-tty" >> ~/.gnupg/gpg.conf
