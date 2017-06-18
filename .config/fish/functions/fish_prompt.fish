7c7945d66d00c2c069a1f5e9aaadc06687157d9a
davinkevin <davin.kevin@gmail.com>
2017-06-18 15:37:18 +0200

    feat(prompt): add branch name in the prompt if in a git repository

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -4,5 +4,9 @@ function fish_prompt
         set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
 
-    echo -n (set_color --bold green)"$__fish_prompt_hostname"":"(set_color --bold white)(prompt_pwd) (set_color --bold cyan)(whoami)(set_color normal)' $ '
+    if not set -q __git_cb
+        set __git_cb " | "(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+    end
+
+    echo -n -s (set_color --bold green) "$__fish_prompt_hostname" ":" (set_color --bold grey) (prompt_pwd) (set_color --bold cyan) " " (whoami) (set_color normal) "$__git_cb" ' $ '
 end
