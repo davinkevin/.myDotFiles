6cfcd2113967b5381a42d62f14d83e9aeb8772b9
davinkevin <davin.kevin@gmail.com>
2017-07-04 05:07:35 +0200

    feat(http-server): use simpleHttp2Server to serve file from the current location
    Closes #3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,12 @@ set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH ./node_modules/.bin/
 # Autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
+# iTerm2
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 # DirEnv 
 eval (direnv hook fish)
+
+# SimpleHttp2Server
+alias http-server="simplehttp2server"
+
---
+++ install/macos/simplehttp2server.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+echo "Install simplehttp2server"
+brew tap GoogleChrome/simplehttp2server https://github.com/GoogleChrome/eimplehttp2server
+brew install simplehttp2server
