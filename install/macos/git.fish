0bb6bb43ff8854d0bbe4b80aae1460db6b22ec31
davinkevin <davin.kevin@gmail.com>
2017-09-23 17:40:55 +0200

    feat(git): add configuration and aliases to git
    Closes #8

--- install/macos/git.fish
+++ install/macos/git.fish
@@ -17,6 +17,23 @@ echo "Install diff-so-fancy"
 brew install diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
+#######################
+# Git Global config   #
+#######################
+
+git config --global user.name davinkevin
+git config --global user.email davin.kevin@gmail.com
+git config --global core.excludesfile ~/.gitignore_global
+
+git config --global alias.br branch
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.list 'log --oneline --decorate'
+git config --global alias.rb rebase
+git config --global alias.list-alias 'config --get-regexp alias'
+git config --global alias.trash 'checkout -- .'
+git config --global alias.graph 'log --all --decorate --oneline --graph'
 
 #####################
 # Git GPG Signature #
